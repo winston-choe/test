d23c226754820d0f8c36a4cd599d6424a319e0eb
winston <winston.choe@fieldcure.com>
2022-03-11 10:55:27 +0900

    second commit

--- blink/blink.c
+++ blink/blink.c
@@ -15,9 +15,9 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(500);
+        sleep_ms(1000);
         gpio_put(LED_PIN, 0);
-        sleep_ms(100);
+        sleep_ms(200);
     }
 #endif
 }
